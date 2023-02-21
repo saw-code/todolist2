b4c1cb49172d359211b494a3b42e9695eaa9ee25
Dmitry Pilavskiy <saw.coding@gmail.com>
2023-02-21 13:29:22 +0300

    before start make tests and reducers

---
+++ src/state/user-reducer.ts
@@ -0,0 +1,10 @@
+export const userReducer = (state: any, action: any) => {
+  switch (action.type) {
+    case "BLABLA1":
+
+    case "YO":
+
+    default:
+      throw new Error("I Don't understand this action type")
+  }
+}
